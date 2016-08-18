d9e5367831ee5ea1853008a612c2a210743cc540
holgerd77 <Holger.Drewes@googlemail.com>
2016-08-18 20:34:52 +0200

    Finally works as expected

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # tageseltern-wien-scraper
 Scraper for day parents in Vienna
 
-Run with ``scrapy runspider tageseltern.py``.
+Run with ``scrapy runspider --output=wien_tageseltern.csv --output-format=csv tageseltern.py``.
 
 
--- tageseltern.py
+++ tageseltern.py
@@ -1,15 +1,21 @@
-import scrapy
+import time, scrapy
 
 class TageselternSpider(scrapy.Spider):
     name = 'tageseltern'
     start_urls = ['https://www.kinderdrehscheibe.at/de/beratung/online-suche-wiener-kinderbetreuung/']
     
-    def after_post(self, response):
+    def parse(self, response):
         #print(response.body)
-        for item_sel in response.xpath('//div[@id="platzsearchresults"]/div[@class="item"]'):
+        items_sel = response.xpath('//div[@id="platzsearchresults"]/div[@class="item"]')
+        for item_sel in items_sel:
             title = item_sel.xpath('.//tr[1]//h2/text()').extract()[0].strip()
+            last_update = item_sel.xpath('.//tr[4]/td[2]/text()').extract()
+            if len(last_update) > 0:
+                last_update = last_update[0]
+            else:
+                last_update = "Not available"
             if 'in:' in title:
-                title = title[:-5]
+                title = title[:-4]
                 location = item_sel.xpath('.//tr[1]//span[@class="black"]/text()').extract()[0]
             else:
                 location = item_sel.xpath('.//tr[2]/td[1]/text()').extract()[0].strip()
@@ -18,12 +24,14 @@ class TageselternSpider(scrapy.Spider):
                 'location': location,
                 'plz': location[0:4],
                 'available': item_sel.xpath('.//tr[4]/td[1]/text()').extract()[0].strip(),
-                'last_update': item_sel.xpath('.//tr[4]/td[2]/text()').extract()[1],
+                'last_update': last_update,
             }
             print(item_array)
+            yield item_array
     
-    def parse(self, response):
-        plz_list = ['1130', '1140',]
+    def start_requests(self):
+        plz_list = ["1%02d0" % i for i in range(0, 24)]
+        start_requests = []
         for plz in plz_list:
             formdata = {
                 'initplatzsearch': '1',
@@ -33,6 +41,6 @@ class TageselternSpider(scrapy.Spider):
                 'art[tm]': '1',
                 'SUBMIT': 'Betreuungsplätze suchen',
             }
-            return [scrapy.http.FormRequest(url=self.start_urls[0],
+            yield scrapy.http.FormRequest(url=self.start_urls[0],
                         formdata=formdata,
-                        callback=self.after_post)]
+                        dont_filter=True)
